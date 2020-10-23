78a50c2721974fee1151d0a5cd0920f10ef9c4e9
Bensille Bruce Aburiyna <bensille.aburinya@amalitech.org>
2020-10-23 07:17:59 +0000

    Functions

--- basics-01-start-proj/assets/scripts/app.js
+++ basics-01-start-proj/assets/scripts/app.js
@@ -1,9 +1,16 @@
-alert('It works; app.js');
-
 let currentResult = 0;
 
 currentResult += 10;
 
-outputResult(currentResult, '');
+function add(num1, num2){
+  const result = num1 + num2;
+  alert(`The result is ${result}`);
+}
 
-alert(currentResult);
+add(1, 20);
+add(20, 20);
+add(30, 20);
+
+//let calcDescription = `(${currentResult} + 10)`;
+
+//outputResult(currentResult, calcDescription);
