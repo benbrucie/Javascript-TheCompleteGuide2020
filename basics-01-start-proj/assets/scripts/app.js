2e7a0fd869b955fd96c29954880a897bb34bdcf3
Bensille Bruce Aburiyna <bensille.aburinya@amalitech.org>
2020-10-23 11:00:55 +0000

    Working calculator

--- basics-01-start-proj/assets/scripts/app.js
+++ basics-01-start-proj/assets/scripts/app.js
@@ -4,11 +4,42 @@ function getInputNum(){
   return parseInt(userInput.value);
 }
 
-function add(){
-  let enteredNum = getInputNum();
-  let description = `${currentResult} + ${enteredNum}`;
-  currentResult = currentResult + parseInt(enteredNum);
+function writeDescription(operator, firstResult, resultNow){
+  const description = `${firstResult} ${operator} ${resultNow}`;
   outputResult(currentResult, description);
 }
 
+function add(){
+  const enteredNum = getInputNum();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNum;
+  writeDescription('+', initialResult, enteredNum);
+}
+
+function subtract(){
+  const enteredNum = getInputNum();
+  const initialResult = currentResult;
+  currentResult = currentResult - parseInt(enteredNum);
+  writeDescription('-', initialResult, enteredNum);
+}
+
+function multiply(){
+  const enteredNum = getInputNum();
+  const initialResult = currentResult;
+  currentResult = currentResult * parseInt(enteredNum);
+  writeDescription('*', initialResult, enteredNum);
+}
+
+function divide(){
+  const enteredNum = getInputNum();
+  const initialResult = currentResult;
+  currentResult = currentResult / parseInt(enteredNum);
+  writeDescription('/', initialResult, enteredNum);
+}
+
+
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
