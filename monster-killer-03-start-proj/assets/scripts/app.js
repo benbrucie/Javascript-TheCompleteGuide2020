be5d9ad83cb553c1d2352c6d8dd3131e0a57cca4
Bensille Bruce Aburiyna <bensille.aburinya@amalitech.org>
2020-10-28 13:39:43 +0000

    attack button

---
+++ monster-killer-03-start-proj/assets/scripts/app.js
@@ -0,0 +1,14 @@
+const ATTACK_VALUE = 10;
+
+let chosenMaxlife = 100;
+let currentMonsterHealth = chosenMaxlife;
+let currentPlayerHealth = chosenMaxlife;
+
+adjustHealthBars(chosenMaxlife);
+
+function attackHandler(){
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+}
+
+attackBtn.addEventListener('click', attackHandler);
