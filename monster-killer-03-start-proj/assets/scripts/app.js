a6765cdc2af00ba326939039bf1bcef116e8756b
Bensille Bruce Aburiyna <bensille.aburinya@amalitech.org>
2020-10-28 16:07:35 +0000

    create strong attack btn with reusable code

--- monster-killer-03-start-proj/assets/scripts/app.js
+++ monster-killer-03-start-proj/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const SRONG_ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 10;
 
 let chosenMaxlife = 100;
@@ -7,13 +8,17 @@ let currentPlayerHealth = chosenMaxlife;
 
 adjustHealthBars(chosenMaxlife);
 
-function attackHandler(){
-  const monDamage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode){
+  let maxDamage;
+  if (mode ==='ATTACK'){
+    maxDamage = ATTACK_VALUE;
+  }else if (mode === 'STRONG_ATTACK'){
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const monDamage = dealMonsterDamage(maxDamage);
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-
   currentMonsterHealth -= monDamage;
   currentPlayerHealth -= playerDamage;
-
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0){
     alert('You won!');
   } else if (currentPlayerHealth <=0 && currentMonsterHealth > 0){
@@ -23,4 +28,13 @@ function attackHandler(){
   }
 }
 
+function attackHandler(){
+  attackMonster('ATTACK');
+}
+
+function strongAttackHandler(){
+  attackMonster('STRONG_ATTACK');
+}
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
