3cc688aa5c67d47de29f0546ac0edda6d1cc25b6
Bensille Bruce Aburiyna <bensille.aburinya@amalitech.org>
2020-10-31 14:28:37 +0000

    assignment 6

---
+++ dom-assignment-6-start/assignment.js
@@ -0,0 +1,15 @@
+const task1 = document.getElementById('task-1');
+
+const task11 = document.querySelector('#task-1');
+
+task1.style.color = 'white';
+task11.style.backgroundColor = 'black';
+
+const myTitle = document.querySelector('title');
+//myTitle.textContent = 'Assignment - Solved!';
+
+const myTitle2 = document.head.querySelector('title');
+myTitle2.textContent = 'Assignment - Solved!';
+
+const h1 = document.querySelector('h1');
+h1.textContent = 'Assignment - Solved!';
